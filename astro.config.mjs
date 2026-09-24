3f5bbc490c10d355f8a25a331bfa1d544ae7c7d2
Vinayak Hegde <hegdevinayak008@gmail.com>
2026-09-24 12:01:12 +0530

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  site: process.env.PUBLIC_SITE_URL || 'http://localhost:4321',
+  site: 'https://voidstack-eta.vercel.app',
+  integrations: [sitemap()],
 });
